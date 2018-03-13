3395c9c1d1c8a7371621ab9f4840ab2806b83d2b
Haifa <haifaghrab@gmail.com>
2018-03-13 21:12:46 +0100

    Initial application generated by JHipster-4.14.0

---
+++ src/main/java/com/pfa/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.pfa.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Mypfaapp.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
